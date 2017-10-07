30a481146f05b7d679e0cb05c588f9d43d39d568
Josephine Stockton <nerdystockton@gmail.com>
2017-10-07 17:04:24 -0600

    Changed Var to Let.

--- js/code.js
+++ js/code.js
@@ -1,5 +1,5 @@
-var allowedElements = [];
-var sidebarElements = [];
+let allowedElements = [];
+let sidebarElements = [];
 
 function allowPrimaryElements(debug)
 {
@@ -7,7 +7,7 @@ function allowPrimaryElements(debug)
    {
     return;
    }
-    for (var index = 0; index < elements.length; index++) {
+    for (let index = 0; index < elements.length; index++) {
         if (elements[index].primary !== true && !debug) {
 
             continue;
@@ -18,15 +18,15 @@ function allowPrimaryElements(debug)
 
 
 function combine(element1, element2) {
-    var results = [];
-    for (var index = 0; index < elements.length; index++) {
-        var element = elements[index];
-        var parents = element.parents;
+    let results = [];
+    for (let index = 0; index < elements.length; index++) {
+        let element = elements[index];
+        let parents = element.parents;
         if (parents.length == 0) {
             continue;
         }
-        for (var parentIndex = 0; parentIndex < parents.length; parentIndex++) {
-            var parent = parents[parentIndex];
+        for (let parentIndex = 0; parentIndex < parents.length; parentIndex++) {
+            let parent = parents[parentIndex];
             if ((element1 == parent[0] && element2 == parent[1]) || (element1 == parent[1] && element2 == parent[0])) {
                 results.push(element);
             }
@@ -36,8 +36,8 @@ function combine(element1, element2) {
 }
 
 function findElementByName(name) {
-    for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
+    for (let i = 0; i < elements.length; i++) {
+        let element = elements[i];
         if (element.name.toLowerCase().trim() == name.toLowerCase().trim()) {
             // I found it
             return element;
@@ -48,8 +48,8 @@ function findElementByName(name) {
 }
 
 function findElementById(id) {
-    for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
+    for (let i = 0; i < elements.length; i++) {
+        let element = elements[i];
         if (element.id == id) {
             return element;
         }
@@ -59,8 +59,8 @@ function findElementById(id) {
 }
 
 function combineByName(name1, name2) {
-    var element1 = findElementByName(name1);
-    var element2 = findElementByName(name2);
+    let element1 = findElementByName(name1);
+    let element2 = findElementByName(name2);
     if (element1 === false || element2 === false) {
         return [];
     }
@@ -70,43 +70,43 @@ function combineByName(name1, name2) {
 function displayPrimaryElements()
 {
     allowPrimaryElements();
-    for(var i = 0; i < allowedElements.length; i++) {
-        var elementId   = allowedElements[i];
-        var element     = findElementById(elementId);
-        var elementName = element.name;
+    for(let i = 0; i < allowedElements.length; i++) {
+        let elementId   = allowedElements[i];
+        let element     = findElementById(elementId);
+        let elementName = element.name;
         addToSidebar($(displayElement(elementName)).appendTo(sidebar));
     }
 }
 
 function displayAllElements()
 {
-    for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        var elementName = element.name;
+    for (let i = 0; i < elements.length; i++) {
+        let element = elements[i];
+        let elementName = element.name;
         $(displayElement(elementName)).appendTo('#playfield');
     }
 }
 
 function duplicateElement(element)
 {
-    var theName = $(element).attr('element-type');
-    var theElement = displayElement(theName);
+    let theName = $(element).attr('element-type');
+    let theElement = displayElement(theName);
     theElement.appendTo('#playfield');
     return theElement;
 }
 
 function displayElement(elementName)
 {
-    // var playfield = $('#playfield');
-    var theDiv = $('<div/>', {
+    // let playfield = $('#playfield');
+    let theDiv = $('<div/>', {
         'class': 'element ' + elementName
     }).draggable({ stack: ".element"});
 
     theDiv.text(elementName);
 
     theDiv.dblclick(function (event) {
-        var theName = $(this).attr('element-type');
-        var element = displayElement(theName);
+        let theName = $(this).attr('element-type');
+        let element = displayElement(theName);
         element.appendTo('#playfield');
         // console.log(event);
     });
@@ -115,17 +115,17 @@ function displayElement(elementName)
         hoverClass: 'hovering',
         greedy: true,
         drop: function(event, ui) {
-            var droppedOn = ($(this).attr('element-type'));
-            var dropped = ($(ui.draggable).attr('element-type'));
+            let droppedOn = ($(this).attr('element-type'));
+            let dropped = ($(ui.draggable).attr('element-type'));
             // console.log(dropped + " was dropped on " + droppedOn);
 
-            var results = combineByName(dropped, droppedOn);
+            let results = combineByName(dropped, droppedOn);
             if (results.length != 0) {
                 // remove the elements
                 $(this).fadeOut().remove();
                 $(ui.draggable).fadeOut().remove();
             }
-            for (var i = 0; i < results.length; i++) {
+            for (let i = 0; i < results.length; i++) {
                 displayElement(results[i].name).appendTo('#playfield');
                 addToSidebar(displayElement(results[i].name));
                 // console.log(results[i].name + ' was created.');
@@ -146,12 +146,12 @@ function cheat(name1, name2) {
 
 function dropCombine(dropped, droppedOn)
 {
-    var results = combineByName(dropped.attr('element-type'), droppedOn.attr('element-type'));
+    let results = combineByName(dropped.attr('element-type'), droppedOn.attr('element-type'));
     if (results.length != 0) {
         $(dropped).fadeOut().remove();
         $(droppedOn).fadeOut().remove();
     }
-    for (var i = 0; i < results.length; i++) {
+    for (let i = 0; i < results.length; i++) {
         displayElement(results[i].name).appendTo('#playfield');
         addToSidebar(displayElement(results[i].name));
     }
@@ -180,8 +180,8 @@ function addToSidebar(element)
 
 function inSidebar(element)
 {
-    var elementName = $(element).attr('element-type');
-    for (var i = 0; i < sidebarElements.length; i++) {
+    let elementName = $(element).attr('element-type');
+    for (let i = 0; i < sidebarElements.length; i++) {
         if (elementName == sidebarElements[i]) {
             return true;
         }
@@ -201,7 +201,7 @@ function prepareGarbage()
 {
     $('#trash').droppable({
         drop: function(event, ui) {
-            var dropped = $(ui.draggable);
+            let dropped = $(ui.draggable);
             $(dropped).toggle('explode').remove();
             playSound('no_want');
         }
@@ -228,7 +228,7 @@ $(function() {
     displayPrimaryElements();
 });
 
-var sounds = [];
+let sounds = [];
 
 
 function playSound(sound)
